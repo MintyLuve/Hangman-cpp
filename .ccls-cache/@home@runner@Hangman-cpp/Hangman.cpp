2d07ba56e6495e7d4634567a4ba31fad219e29a1
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-04-11 15:16:59 +0000

    made layout plan

--- .ccls-cache/@home@runner@Hangman-cpp/Hangman.cpp
+++ .ccls-cache/@home@runner@Hangman-cpp/Hangman.cpp
@@ -1,4 +1,48 @@
-int main() {
-    
+#include <iostream>
+using namespace std;
+
+string words[30]{
+"","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
+};
+
+int main()
+{
+    //creates the game, uses all of the functions, yeah
+
     return 0;
+}
+
+int getWord(){
+    /*gets a random index
+    return index;*/
+}
+bool isCorrect(char input){
+    /*checks if the input is correct
+    if (correct){
+        return true;
+    }
+    if (incorrect){
+        return false;
+    }*/
+}
+void noWrong(){
+    //cout<<
+}
+void oneWrong(){
+    //cout<<
+}
+void twoWrong(){
+    //cout<<
+}
+void threeWrong(){
+    //cout<<
+}
+void fourWrong(){
+    //cout<<
+}
+void fiveWrong(){
+    //cout<<
+}
+void sixWrong(){
+    //cout<<
 }
